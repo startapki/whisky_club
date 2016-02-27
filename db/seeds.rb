e466605be6b77b33d0779884ec8ab67700596bbe
Yevhen Shemet <yevhene@gmail.com>
2016-02-28 00:26:41 +0200

    Examples commented out from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,28 +2,30 @@ User.destroy_all
 Meeting.destroy_all
 
 User.create!(
-  email: 'user@example.com',
-  password: 'password',
-  password_confirmation: 'password'
-)
-
-User.create!(
   email: 'admin@example.com',
   admin: true,
   password: 'password',
   password_confirmation: 'password'
 )
 
-meeting = Meeting.create!(
-  title: 'Hello'
-)
+# User.create!(
+#   email: 'user@example.com',
+#   password: 'password',
+#   password_confirmation: 'password'
+# )
 
-images_path = 'test/fixtures/files/item/image/*'
-images = Dir.glob(Rails.root.join(images_path)).sort.cycle
+# meeting = Meeting.create!(
+#   title: 'Hello'
+# )
 
-Item.create!(
-  title: 'Example item',
-  description: 'Lorem ipsum **dolore** sit amet',
-  meeting: meeting,
-  image: File.open(images.next)
-)
+# images_path = 'test/fixtures/files/item/image/*'
+# images = Dir.glob(Rails.root.join(images_path)).sort.cycle
+
+# 5.times do
+#   Item.create!(
+#     title: 'Example item',
+#     description: 'Lorem ipsum **dolore** sit amet',
+#     meeting: meeting,
+#     image: File.open(images.next)
+#   )
+# end
