671db4ca20ee73bb6e178f40eb4fffc0930c3d99
Pavel Druziak <pavel.druzyak@gmail.com>
2016-02-27 18:10:58 +0200

    Improve item model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227155431) do
+ActiveRecord::Schema.define(version: 20160227160900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 20160227155431) do
   create_table "items", force: :cascade do |t|
     t.text     "title"
     t.text     "description"
-    t.integer  "user_id"
     t.integer  "position"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "meeting_id"
   end
 
-  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
+  add_index "items", ["meeting_id"], name: "index_items_on_meeting_id", using: :btree
 
   create_table "meetings", force: :cascade do |t|
     t.string   "title"
@@ -62,5 +62,5 @@ ActiveRecord::Schema.define(version: 20160227155431) do
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  add_foreign_key "items", "users"
+  add_foreign_key "items", "meetings"
 end
