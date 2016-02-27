dfbb45b4fd6432cb2c775339fd39663603371bea
Yevhen Shemet <yevhene@gmail.com>
2016-02-27 21:20:05 +0200

    Stars fixed

--- db/migrate/20160227172937_create_ratings.rb
+++ db/migrate/20160227172937_create_ratings.rb
@@ -1,7 +1,7 @@
 class CreateRatings < ActiveRecord::Migration[5.0]
   def change
     create_table :ratings do |t|
-      t.integer :value
+      t.float :value
       t.text :comment
 
       t.references :user, foreign_key: true, null: false
