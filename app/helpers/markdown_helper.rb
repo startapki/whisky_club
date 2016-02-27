6d8e20854d407cfc0fe65d65b2929e7ac952a6cf
Yevhen Shemet <yevhene@gmail.com>
2016-02-27 19:22:22 +0200

    Policies

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,21 +1,21 @@
 module MarkdownHelper
+  OPTIONS = {
+    filter_html: true,
+    hard_wrap: true,
+    link_attributes: { rel: 'nofollow', target: '_blank' },
+    space_after_headers: true,
+    fenced_code_blocks: true
+  }.freeze
+
+  EXTENSIONS = {
+    autolink: true,
+    superscript: true,
+    disable_indented_code_blocks: true
+  }.freeze
+
   def markdown(text)
-    options = {
-      filter_html:     true,
-      hard_wrap:       true,
-      link_attributes: { rel: 'nofollow', target: "_blank" },
-      space_after_headers: true,
-      fenced_code_blocks: true
-    }
-
-    extensions = {
-      autolink:           true,
-      superscript:        true,
-      disable_indented_code_blocks: true
-    }
-
-    renderer = Redcarpet::Render::HTML.new(options)
-    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    renderer = Redcarpet::Render::HTML.new(OPTIONS)
+    markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
 
     markdown.render(text).html_safe
   end
