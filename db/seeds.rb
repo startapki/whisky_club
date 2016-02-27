d982f483900458d224e795d02d4dd1291b564bf5
Yevhen Shemet <yevhene@gmail.com>
2016-02-27 19:14:26 +0200

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,17 @@ User.create!(
   password: 'password',
   password_confirmation: 'password'
 )
+
+meeting = Meeting.create!(
+  title: 'Hello'
+)
+
+images_path = 'test/fixtures/files/item/image/*'
+images = Dir.glob(Rails.root.join(images_path)).sort.cycle
+
+Item.create!(
+  title: 'Example item',
+  description: 'Lorem ipsum **dolore** sit amet',
+  meeting: meeting,
+  image: File.open(images.next)
+)
