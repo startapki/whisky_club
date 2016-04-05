de589c8280f2772dd8a9849183a944c962722823
Pavel Druziak <pavel.druzyak@gmail.com>
2016-04-05 16:53:21 +0300

    Refactoring Meeting#archive

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,8 +1,5 @@
 class Meeting < ApplicationRecord
   has_many :items, -> { order(position: :asc) }, dependent: :destroy
 
-  def self.archive
-    return [] if last.nil?
-    where.not(id: last.id)
-  end
+  scope :archive, -> { where.not(id: last&.id) }
 end
