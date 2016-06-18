799040eac1f9e13bcceb41e602aec3e18ae04146
Pavel Druziak <pavel.druzyak@gmail.com>
2016-06-18 19:19:13 +0300

    Simple filters for archvie

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,19 @@
+class ItemsController < ApplicationController
+  def index
+    @items = if params[:attribute_kind_id].blank?
+               Item.all
+             else
+               mixpanel.track('Filter Archive')
+               filter_by_attribute(params[:attribute_kind_id], params[:value])
+             end
+    mixpanel.track('View Archive')
+  end
+
+  private
+
+  def filter_by_attribute(attribute_kind_id, value)
+    AttributeKind.find(attribute_kind_id).attribute_values.map do |a_value|
+      a_value.item if a_value.value == value
+    end.compact
+  end
+end
